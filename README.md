3f43b7e97ae0c85650609a3b9319e8005d90c3dd
Gabor Udvari <gabor.udvari@gmail.com>
2015-08-07 23:49:18 +0200

    Update readme and add initial files

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# phlomis
+# Phlomis
+
+Phlomis is a project for replacing the Node.js based build tools of [Sage](https://github.com/roots/sage) to a PHP based one with [Composer](https://getcomposer.org/) and [Robo](http://codegyre.github.io/Robo/).
+
+The name comes from the genus of plants called [Phlomis](https://en.wikipedia.org/wiki/Phlomis). One of the members of the genus is Phlomis russeliana which has the common name Turkish sage. That and the "Ph" at the beginning makes it a perfect name for a PHP oriented fork of Sage.
+
+## Requirements
+
+| Prerequisite    | How to check | How to install
+| --------------- | ------------ | ------------- |
+| PHP >= 5.4.x    | `php -v`     | [php.net](http://php.net/manual/en/install.php) |
+
+For more installation notes, refer to the [Install gulp and Bower](#install-gulp-and-bower) section in this document.
+
+## Features
+
+* [Sass](https://github.com/twbs/bootstrap-sass) [Bootstrap](http://getbootstrap.com/)
+* [Theme wrapper](https://roots.io/sage/docs/theme-wrapper/)
+* ARIA roles and microformats
+* Posts use the [hNews](http://microformats.org/wiki/hnews) microformat
+* [Multilingual ready](https://roots.io/wpml/) and over 30 available [community translations](https://github.com/roots/sage-translations)
+
+Install the [Soil](https://github.com/roots/soil) plugin to enable additional features:
+
+* Cleaner output of `wp_head` and enqueued assets
+* Cleaner HTML output of navigation menus
+* Root relative URLs
+* Nice search (`/search/query/`)
+* Google CDN jQuery snippet from [HTML5 Boilerplate](http://html5boilerplate.com/)
+* Google Analytics snippet from [HTML5 Boilerplate](http://html5boilerplate.com/)
+
+## Documentation
+
+Sage documentation is available at [https://roots.io/sage/docs/](https://roots.io/sage/docs/).
---
+++ RoboFile.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * This is Phlomis project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    use MyScss;
+
+    public function __construct () {
+        // check the build folder
+        if ( ! is_dir('dist') ) {
+            mkdir('dist');
+        }
+        if ( ! is_dir('dist/styles') ) {
+            mkdir('dist/styles');
+        }
+    }
+
+    // ### Styles
+    // `gulp styles` - Compiles, combines, and optimizes Bower CSS and project CSS.
+    // By default this task will only log a warning if a precompiler error is
+    // raised. If the `--production` flag is set: this task will fail outright.
+    public function styles () {
+        // compile LESS to CSS
+            // 'vendor/bower-asset/bootstrap-sass-official/assets/stylesheets/_bootstrap.scss' => 'compiled.css'
+        $this->taskMyScss([
+            'assets/styles/main.scss' => 'dist/styles/main.css'
+        ])
+        ->compiler('myscss')
+        ->run();
+
+        /*
+        // concat compiled CSS with info CSS
+        $this->taskConcat([
+            '_src_/css/udionline-less/info.less',
+            '_src_/css/compiled.css'
+        ])
+        ->to('style.css')
+        ->run();
+
+        // remove temporary CSS
+        $this->taskFileSystemStack()
+            ->remove('_src_/css/compiled.css')
+            ->run();
+         */
+    }
+}
+
+trait MyScss
+{
+    function taskMyScss($input)
+    {
+        return new MyScssTask($input);
+    }
+}
+
+class MyScssTask extends \Robo\Task\Assets\Less
+{
+    protected function myscss($file)
+    {
+        $scssCode = file_get_contents($file);
+        /*
+        $parser = new \Less_Parser();
+        $directories = array(
+            '/var/www/udionline/wp-content/themes/udionline/_src_/css/udionline-less'=>'../udionline-less',
+            '/var/www/udionline/wp-content/themes/udionline/_src_/css/less'=>'./'
+        );
+        $options = array( 'import_callback'=>'import_callback_function' );
+         */
+        $scss = new \Leafo\ScssPhp\Compiler();
+        $scss->setImportPaths([
+            'vendor/bower-asset/bootstrap-sass-official/assets/stylesheets'
+        ]);
+        /*
+        $scss->addImportPath(function($path) {
+            $file = basename($path);
+            $dir = 'vendor/bower-asset/bootstrap-sass-official/assets/stylesheets/bootstrap';
+            if (!file_exists($dir . '/_'.$file.'.scss')) { 
+                return null;
+            }
+            return $dir . '/_'.$file.'.scss';
+        });
+         */
+        // $parser->SetImportDirs( $directories );
+        //$parser->parse($lessCode);
+        // $parser->parseFile($file);
+        return $scss->compile($scssCode);
+    }
+
+    protected function lessCompilers()
+    {
+        return [
+            'lessphp', //https://github.com/leafo/lessphp
+            'less', // https://github.com/oyejorge/less.php,
+            'myless', // https://github.com/oyejorge/less.php, but with advanced options
+            'myscss' // https://github.com/leafo/scssphp
+        ];
+    }
+}
