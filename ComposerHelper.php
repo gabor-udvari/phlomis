cd7090a1569605baab9d6885066a179c456b7247
Gábor Udvari <gabor.udvari@gmail.com>
2018-01-28 14:08:32 +0100

    Remove deprecated warnings from composer.json

--- ComposerHelper.php
+++ ComposerHelper.php
@@ -7,15 +7,19 @@ class ComposerHelper
   public static function skipAssets(Event $event)
   {
     $composer = $event->getComposer();
-    $requires = $composer->getPackage()->getRequires();
+    $requires['req'] = $composer->getPackage()->getRequires();
+    $requires['dev'] = $composer->getPackage()->getDevRequires();
 
-    foreach ($requires as $key => $value) {
-      // delete every bower-asset requirement
-      if (preg_match('/bower-asset\/.*/', $key)) {
-        unset($requires[$key]);
+    foreach ($requires as $type_key => $type) {
+      foreach ($type as $key => $value) {
+        // delete every bower-asset requirement
+        if (preg_match('/bower-asset\/.*/', $key)) {
+          unset($requires[$type_key][$key]);
+        }
       }
     }
 
-    $composer->getPackage()->setRequires($requires);
+    $composer->getPackage()->setRequires($requires['req']);
+    $composer->getPackage()->setDevRequires($requires['dev']);
   }
 }
